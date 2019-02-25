f0f241bfdabde37793afbe790739a89a94fcb598
Coursera Learner <nobody@nowhere.nul>
2019-02-25 06:06:43 +0000

    completing assignment 6

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,15 +16,61 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
+struct rect_tag{
+  int x;
+  int y;
+  int width;
+  int height;
+};
+typedef struct rect_tag rectangle;
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+  if (r.height <0){
+    r.height = abs(r.height);
+    r.y = r.y - r.height; 
+    //printf("r.height=%d\n",r.height);
+    //printf("r.y=%d\n", r.y);
+  }
+  if (r.width <0){
+    r.width = abs(r.height);
+    r.x = r.x - r.width;
+    //printf("r.width=%d\n",r.width);
+    //printf("r.x=%d\n", r.x);
+  }
   return r;
 }
+
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
-  return r1;
+  rectangle r_ans;
+  //Find the left side
+  r_ans.x = max(r1.x, r2.x);
+  //Find the bottom
+  r_ans.y = max(r1.y, r2.y);
+  //Check if they don't intersect
+  if ((r2.y > (r1.y + r1.height)) || ((r2.y + r2.height) < r1.y)){
+    //R2 is completely above or completely below R1
+    r_ans.height = 0;
+    r_ans.width = 0;
+    return r_ans;
+  }
+  if ((r2.x > (r1.x + r1.width)) || ((r2.x + r2.width < r1.x))){
+    //R2 is completely to the left or right of R1
+    r_ans.width = 0;
+    r_ans.height = 0;
+    return r_ans;
+  }
+  
+  //Find the right side
+  int right = min(r1.x + r1.width, r2.x + r2.width);
+  r_ans.width = right - r_ans.x;
+  //Find the top
+  int top = min(r1.y + r1.height, r2.y + r2.height);
+  r_ans.height = top - r_ans.y;
+
+  //NOTE: add case when rectangles don't overlap
+  return r_ans;
 }
 
 //You should not need to modify any code below this line
@@ -52,23 +98,23 @@ int main (void) {
   printf("r1 is ");
   printRectangle(r1);
 
-  r2.x = 4;
-  r2.y = 5;
-  r2.width = -5;
-  r2.height = -7;
+  r2.x = -1; //4;
+  r2.y = -2; //5;
+  r2.width = 5; //-5;
+  r2.height = 7; //-7;
   printf("r2 is ");
   printRectangle(r2);
   
   r3.x = -2;
-  r3.y = 7;
+  r3.y = -3; //7;
   r3.width = 7;
-  r3.height = -10;
+  r3.height = 10; //-10;
   printf("r3 is ");
   printRectangle(r3);
 
-  r4.x = 0;
+  r4.x = -4; //0;
   r4.y = 7;
-  r4.width = -4;
+  r4.width = 4; //-4;
   r4.height = 2;
   printf("r4 is ");
   printRectangle(r4);
