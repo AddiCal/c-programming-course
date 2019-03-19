03264f79c4627e34f1b2a7716e9b8bb474dbdcf8
Coursera Learner <nobody@nowhere.nul>
2019-03-19 17:33:50 +0000

    completing assignment 20

---
+++ 20_rot_matrix/rotate.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void transpose(char matrix[10][10]){
+  char temp1;
+  char temp2;
+  for (int i=0; i<10; i++){
+    for (int j=0; j<10; j++){
+      if (j > i){
+	temp1 = matrix[i][j];
+	matrix[i][j] = matrix[j][i];
+	matrix[j][i] = temp1;
+      }
+    }
+  }
+}
+
+void reverse(char matrix[10][10]){
+  char swap;
+  int count = 10-1;
+  for (int i=0; i<10; i++){
+    for (int j=0; j<5; j++){
+      swap = matrix[i][j];
+      matrix[i][j] = matrix[i][count-j];
+      matrix[i][count-j] = swap;
+    }
+  }
+}
+
+void rotate(char matrix[10][10]){
+  //transpose and then reverse rows
+  transpose(matrix);
+  reverse(matrix);
+}
