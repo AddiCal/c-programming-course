ba60d9516749ca5eda3f894bf291c45879b3b2cf
Coursera Learner <nobody@nowhere.nul>
2019-05-17 04:50:22 +0000

    added back part where fc->decks = newdeck

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -129,9 +129,9 @@ void add_future_card(future_cards_t *fc, size_t index, card_t * ptr){
 
     newDeck->cards[0] = newCards[0];
     newDeck->n_cards = 1;
-    /*
+    
     fc->decks[index] = *newDeck;
-    fc->n_decks = index+1; */
+    //fc->n_decks = index+1;
   } 
 }
 
